976a8a1936abc0f14e02a5dc28148b5d50fbce4f
孙珈瑜 <19220215@njnu.edu.cn>
2022-10-18 17:04:51 +0800

    Add homework-19220215W02

--- 2022W02/2-9.c/2-9.c
+++ 2022W02/2-9.c/2-9.c
@@ -1,10 +1,10 @@
-//https://www.acwing.com/blog/content/26422/
+//https://www.acwing.com/file_system/file/content/whole/index/content/6951623/
 #include<stdio.h>
 int main() {
 #define pi 3.14159
-    double r, A;
-    scanf("%lf", &r);
-    A = pi * r * r;
-    printf("A=%.4lf", A);
-    return 0;
+	double r, A;
+	scanf(" % lf", &r);
+	A = pi*r*r;
+	printf("A = % .4lf", A);
+	return 0;
 }
